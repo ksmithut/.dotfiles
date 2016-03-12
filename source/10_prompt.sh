d5e0dcbe2a33e4b542510f109a9a9470cf6145e5
ksmithut <ksmith.ut@gmail.com>
2016-03-11 17:51:35 -0700

    works fine without ATOM_HOME

--- source/10_prompt.sh
+++ source/10_prompt.sh
@@ -3,6 +3,5 @@ resetprompt
 
 # export environment variables
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
-# export ATOM_HOME=$DOTFILES/link/.atom
 export CLICOLOR=1
 export LSCOLORS=gxfxcxdxhxegedabagacad # http://geoff.greer.fm/lscolors/
