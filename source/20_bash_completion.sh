6b2b8925c33169901b2b81aa2b0f188f16fe2c23
ksmithut <ksmith.ut@gmail.com>
2016-03-22 15:09:49 -0600

    adds npm completion

--- source/20_bash_completion.sh
+++ source/20_bash_completion.sh
@@ -1,5 +1,5 @@
 # include all of the bash_completion stuff
 for f in /usr/local/etc/bash_completion.d/*; do . $f; done;
 
-# npm auto complete
-npm completion
+# npm autocomplete
+eval "`npm completion`"
