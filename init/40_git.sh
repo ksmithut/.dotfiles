52a17365281cdb8df8874eda28f861cca3b1e962
ksmithut <ksmith.ut@gmail.com>
2016-04-17 08:46:01 -0600

    copy public key

--- init/40_git.sh
+++ init/40_git.sh
@@ -12,7 +12,7 @@ git config --global github.token $GITHUB_TOKEN
 ssh-keygen -t rsa -b 4096 -C "$GITHUB_EMAIL"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
-pbcopy < ~/.ssh/id_rsa
+pbcopy < ~/.ssh/id_rsa.pub
 
 echo "Paste in your public key in the opened window"
 open "https://github.com/settings/keys"
