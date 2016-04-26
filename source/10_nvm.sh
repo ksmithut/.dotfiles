8c26413fb3b3cdc60c486bfb5dfbbd87b6b847a9
ksmithut <ksmith.ut@gmail.com>
2016-04-26 15:06:18 -0600

    adds NODEJS_ORG_MIRROR to nvm

--- source/10_nvm.sh
+++ source/10_nvm.sh
@@ -1,3 +1,4 @@
 export NVM_DIR=~/.nvm
 [[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+export NODEJS_ORG_MIRROR="$NVM_NODEJS_ORG_MIRROR"
