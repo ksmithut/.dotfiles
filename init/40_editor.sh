40363791fcfb4796e3fae484eb46839e8daa3359
ksmithut <ksmith.ut@gmail.com>
2016-05-03 16:24:46 -0600

    removes vscode specific stuff from config (using plugin to sync settings)

--- init/40_editor.sh
+++ init/40_editor.sh
@@ -1,7 +1,1 @@
 apm stars --install
-
-# VS Code Specific setup
-VSCODE_SETTINGS_DIR="$HOME/Library/Application Support/Code"
-mkdir -p "$VSCODE_SETTINGS_DIR"
-rm -rf "$VSCODE_SETTINGS_DIR/User"
-ln -s "$DOTFILES/config/vscode" "$VSCODE_SETTINGS_DIR/User"
