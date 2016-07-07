2ea0f2952376cee343fad9ed25d6557d957af913
ksmithut <ksmith.ut@gmail.com>
2016-07-07 15:23:45 -0600

    changes comments for npm completion

--- source/20_bash_completion.sh
+++ source/20_bash_completion.sh
@@ -1,7 +1,9 @@
 # include all of the bash_completion stuff
 for f in /usr/local/etc/bash_completion.d/*; do . $f; done;
 
-# npm autocomplete
 # https://github.com/npm/npm/issues/11696
 export COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
+
+# npm autocomplete
+# generated using `npm completion`
 eval "$(npm completion)"
