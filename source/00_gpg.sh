09900c447da6930035097c222f1b8fab6a9e1d3f
ksmithut <ksmith.ut@gmail.com>
2017-04-07 08:42:23 -0600

    updates gpg stuff

--- source/00_gpg.sh
+++ source/00_gpg.sh
@@ -1,9 +1,1 @@
-# https://github.com/pstadler/keybase-gpg-github
-if test -f ~/.gnupg/.gpg-agent-info -a -n "$(pgrep gpg-agent)"; then
-  source ~/.gnupg/.gpg-agent-info
-  export GPG_AGENT_INFO
-  GPG_TTY=$(tty)
-  export GPG_TTY
-else
-  eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
-fi
+eval $(gpg-agent --daemon > /dev/null 2>&1)
