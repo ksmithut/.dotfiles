c1834b728d45c0442909d47035f21c5d17afa4cd
ksmithut <ksmith.ut@gmail.com>
2017-04-10 08:49:48 -0600

    adds nvm switching

--- source/00_aliases.sh
+++ source/00_aliases.sh
@@ -40,6 +40,27 @@ function mongo-drop-all() {
 # starts simple http server in current directory
 alias static='python -m SimpleHTTPServer'
 
+# Change version of node on cd
+function _change_node_version() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+cd() { builtin cd "$@" && _change_node_version; }
+pushd() { builtin cd "$@" && _change_node_version; }
+popd() { builtin cd "$@" && _change_node_version; }
+
 # reverts to a given commit
 function oh_crap() {
   if [ "$1" = "" ]; then
