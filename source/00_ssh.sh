0637cc8a289d052498cd3e2eaed39a0c2ccd531c
ksmithut <ksmith.ut@gmail.com>
2017-05-03 14:21:41 -0600

    auto adds ssh keys to ssh agent

--- source/00_ssh.sh
+++ source/00_ssh.sh
@@ -1,4 +1,6 @@
 if [ "$(pgrep ssh-agent)" == "" ]; then
   echo "started ssh agent"
   eval $(ssh-agent -s > /dev/null 2>&1)
+  # adds all of the private keys to the agent
+  find ~/.ssh -type f ! -name "*.*" -name id_* -exec ssh-add -K {} \;
 fi
