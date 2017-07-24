2654efe050439d512f19224012ce4f40aba8b0ce
ksmithut <ksmith.ut@gmail.com>
2017-07-24 11:40:41 -0600

    only bash_completion on mac

--- init/00_ubuntu_apt.sh
+++ init/00_ubuntu_apt.sh
@@ -10,4 +10,4 @@ mkdir -p "$DOTFILES/caches/installers/"
 # https://code.visualstudio.com/docs/setup/linux
 curl -L -o "$DOTFILES/caches/installers/vscode.deb" https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable
 sudo dpkg -i "$DOTFILES/caches/installers/vscode.deb"
-sudo apt-get install -f
+sudo apt-get install -fy
--- source/30_bash_completion.sh
+++ source/30_bash_completion.sh
@@ -1,5 +1,5 @@
 # include all of the bash_completion stuff
-for f in /usr/local/etc/bash_completion.d/*; do . $f; done;
+is_macos && for f in /usr/local/etc/bash_completion.d/*; do . $f; done;
 
 # https://github.com/npm/npm/issues/11696
 export COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
