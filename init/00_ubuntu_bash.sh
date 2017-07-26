c8c10d5e8170b5cfc5a31ef9de9e975612d1eae1
ksmithut <ksmith.ut@gmail.com>
2017-07-25 20:06:16 -0600

    moves Ubuntu dock to bottom

--- init/00_ubuntu_bash.sh
+++ init/00_ubuntu_bash.sh
@@ -1,4 +1,8 @@
 # Ubuntu-only stuff. Abort if not OSX.
 is_ubuntu || return 1
 
+# Load .bash_profile on load
 echo 'source ~/.bash_profile' >> ~/.bashrc
+
+# Move dock to bottom
+gsettings set com.canonical.Unity.Launcher launcher-position Bottom
