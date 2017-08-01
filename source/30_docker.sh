7a270b185be09ccb516e0c40ec6c64a42177c14d
ksmithut <ksmith.ut@gmail.com>
2017-08-01 08:13:16 -0600

    adds docker aliases

--- source/30_docker.sh
+++ source/30_docker.sh
@@ -3,13 +3,16 @@
 
 # Elasticsearch
 # auth = elastic:changeme
-alias dock-elasticsearch='docker run -p 9200:9200 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" docker.elastic.co/elasticsearch/elasticsearch:5.5.1'
+alias dock-elasticsearch='echo "username = elastic"; echo "password = changeme"; docker run -p 9200:9200 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" docker.elastic.co/elasticsearch/elasticsearch:5.5.1'
 
 # MongoDB
+# auth = n/a
 alias dock-mongo='docker run -p 27017:27017 mongo:latest'
 
 # Redis
+# auth = n/a
 alias dock-redis='docker run -p 6379:6379 redis:alpine'
 
 # PostgreSQL
-alias dock-postgres='docker run -p 5432:5432 -e POSTGRES_PASSWORD=postgres postgres:alpine'
+# auth = postgres:postgres
+alias dock-postgres='echo "username = postgres"; echo "password = postgres"; docker run -p 5432:5432 -e "POSTGRES_PASSWORD=postgres" postgres:alpine'
