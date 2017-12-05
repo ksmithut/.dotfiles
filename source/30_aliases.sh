ecd52179f679b4586e7fa89ee3040deeed8b1237
ksmithut <ksmith.ut@gmail.com>
2017-12-05 09:31:34 -0700

    add clear command to upgrade ubuntu

--- source/30_aliases.sh
+++ source/30_aliases.sh
@@ -173,7 +173,7 @@ fi
 # ========================
 if is_ubuntu; then
   # upgrade/update shortcuts
-  alias apt-upgrade='sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt-get autoremove -y'
+  alias apt-upgrade='sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt-get autoremove -y; clear'
   alias upgrade='apt-upgrade'
 
   # MD5 Checksum
