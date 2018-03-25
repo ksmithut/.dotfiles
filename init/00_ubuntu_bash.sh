7a27cc5975c091ee292296110d527185220723f6
ksmithut <ksmith.ut@gmail.com>
2018-03-25 15:20:21 -0600

    update chown command

--- init/00_ubuntu_bash.sh
+++ init/00_ubuntu_bash.sh
@@ -1,5 +1,5 @@
 # Ubuntu-only stuff. Abort if not Ubuntu.
 is_ubuntu || return 1
 
-chown -R $USER:$USER /usr/local/bin/
+sudo chown -R $USER:$USER /usr/local/bin/
 echo 'pinentry-program /usr/bin/pinentry-curses' >  ~/.gnupg/gpg-agent.conf
