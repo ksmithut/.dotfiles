379c256eaf91a2fd3bfb1a999bdf3d1f521bdfea
ksmithut <ksmith.ut@gmail.com>
2018-04-11 11:20:18 -0600

    disable docker on ubuntu for now

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -49,28 +49,28 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update -y && sudo apt-get install yarn -y
 
-# docker
-# https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-docker-ce
-sudo apt-get install -y \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-sudo apt-get update -y
-sudo apt-get install docker-ce -y
-# Add current user to docker group
-sudo groupadd docker
-sudo gpasswd -a $USER docker
-sudo usermod -aG docker $USER
-newgrp docker
-# docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/latest/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# # docker
+# # https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-docker-ce
+# sudo apt-get install -y \
+#   apt-transport-https \
+#   ca-certificates \
+#   curl \
+#   software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository \
+#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) \
+#   stable"
+# sudo apt-get update -y
+# sudo apt-get install docker-ce -y
+# # Add current user to docker group
+# sudo groupadd docker
+# sudo gpasswd -a $USER docker
+# sudo usermod -aG docker $USER
+# newgrp docker
+# # docker-compose
+# sudo curl -L https://github.com/docker/compose/releases/download/latest/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 
 # pass and pass otp
 # https://github.com/tadfisher/pass-otp
