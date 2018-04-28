7cfb0a70bc5747dbb3b58716549cdb4629ed32d9
ksmithut <ksmith.ut@gmail.com>
2018-04-28 08:37:06 -0600

    set docker release download to pinned version

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -31,7 +31,7 @@ if is_ubuntu_desktop; then
   git clone https://github.com/gTile/gTile.git ~/.local/share/gnome-shell/extensions/gTile@vibou
 
   # docker-compose
-  sudo curl -L https://github.com/docker/compose/releases/download/latest/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 
   # docker permissions
