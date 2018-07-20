c3a9e8de6c45e67c75e3b78dc94ad5a91864f93b
ksmithut <ksmith.ut@gmail.com>
2018-07-19 21:03:58 -0600

    install docker the "normal" way

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -49,10 +49,14 @@ sudo apt-get install -y \
 sudo apt-get purge -y \
   firefox
 
-sudo snap install docker --classic
-sudo addgroup --system docker
-sudo adduser $USER docker
-newgrp docker
+curl -fsSL get.docker.com -o get-docker.sh
+chmod a+x get-docker.sh
+sudo sh get-docker.sh
+rm get-docker.sh
+sudo usermod -aG docker your-user
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 if is_ubuntu_desktop; then
   # Remove dock
