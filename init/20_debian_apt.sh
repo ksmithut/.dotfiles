6894defda4b5e5017c1d6bc44ac8f294de96e6c3
ksmithut <ksmith.ut@gmail.com>
2018-08-12 14:34:39 +0000

    adds slack download

--- init/20_debian_apt.sh
+++ init/20_debian_apt.sh
@@ -23,12 +23,20 @@ sudo apt-get install -y \
   ca-certificates \
   curl \
   gnupg2 \
-  software-properties-common
+  software-properties-common \
+  firefox-esr
+
+# Slack
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.2.1-amd64.deb -O "$DOTFILES/caches/installers/slack.deb"
+sudo dpkg -i "$DOTFILES/caches/installers/slack.deb"
+sudo apt-get install -f -y
 
 # VSCode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update -y
+sudo apt-get install -y code
 
 # Docker
 sudo apt-get remove docker docker-engine docker.io
@@ -39,6 +47,8 @@ sudo add-apt-repository \
    stable"
 sudo groupadd docker
 sudo usermod -aG docker $USER
+sudo apt-get update -y
+sudo apt-get install -y docker-ce
 
 # docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
@@ -47,9 +57,5 @@ sudo chmod +x /usr/local/bin/docker-compose
 # yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-sudo apt-get update -y
-sudo apt-get install -y code
-sudo apt-get install -y docker-ce
+sudo apt-get update
 sudo apt-get install -y --no-install-recommends yarn
-sudo apt-get install -y firefox-esr
