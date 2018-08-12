27fd630e8811550ddf398169737c89776216cb74
ksmithut <ksmith.ut@gmail.com>
2018-08-12 06:41:26 -0600

    makes ssh-add work on linux

--- source/20_ssh.sh
+++ source/20_ssh.sh
@@ -3,6 +3,10 @@
 if [ "$(pgrep ssh-agent)" == "" ]; then
   eval $(ssh-agent -s > /dev/null 2>&1)
   # adds all of the private keys to the agent
-  eval $(find ~/.ssh -type f ! -name "*.*" -name id_* -exec ssh-add -K {} \; > /dev/null 2>&1)
+  if is_macos; then
+    eval $(find ~/.ssh -type f ! -name "*.*" -name id_* -exec ssh-add -K {} \; > /dev/null 2>&1)
+  else
+    eval $(find ~/.ssh -type f ! -name "*.*" -name id_* -exec ssh-add {} \; > /dev/null 2>&1)
+  fi
   echo "started ssh agent"
 fi
