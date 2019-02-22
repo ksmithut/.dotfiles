257e23b34bc0ae8216f3cc82b3c49f37ad13b4a9
ksmithut <ksmith.ut@gmail.com>
2019-02-22 05:30:10 -0700

    remove uneeded ssh

--- source/20_ssh.sh
+++ source/20_ssh.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 # Start the ssh agent if it hasn't started
-# TODO add .ssh config file, generate new key on install
 if [[ "$(pgrep ssh-agent | head -n 1)" == "" ]]; then
   eval "$(ssh-agent -s > /dev/null 2>&1)"
-  # adds all of the private keys to the agent
-  # if is_macos; then
-  #   eval $(find ~/.ssh -type f ! -name "*.*" -name "id_*" -exec ssh-add -K {} \; > /dev/null 2>&1)
-  # else
-  #   eval $(find ~/.ssh -type f ! -name "*.*" -name "id_*" -exec ssh-add {} \; > /dev/null 2>&1)
-  # fi
   echo "started ssh agent"
 fi
