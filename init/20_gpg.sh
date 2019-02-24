c4726f9c6cd573f4f57080c02b0eb547baf2f4e0
ksmithut <ksmith.ut@gmail.com>
2019-02-24 07:57:39 -0700

    comment out gpg stuff for now

--- init/20_gpg.sh
+++ init/20_gpg.sh
@@ -1,34 +1,35 @@
 #!/usr/bin/env bash
 
 # Skip gpg until I figure out how to do subkeys properly
+return
 
-# eval "$(gpg-agent --daemon > /dev/null 2>&1)"
-# GPG_TTY=$(tty)
-# export GPG_TTY
+eval "$(gpg-agent --daemon > /dev/null 2>&1)"
+GPG_TTY=$(tty)
+export GPG_TTY
 
-# echo "Choose an email address that is tied to your github account"
-# gpg --full-generate-key
+echo "Choose an email address that is tied to your github account"
+gpg --full-generate-key
 
-# KEY_ID="$(gpg --list-secret-keys --keyid-format LONG | grep 'sec   ' | tail -1 | sed -n 's/.*\/\([^ ]*\) .*/\1/p')"
+KEY_ID="$(gpg --list-secret-keys --keyid-format LONG | grep 'sec   ' | tail -1 | sed -n 's/.*\/\([^ ]*\) .*/\1/p')"
 
-# if is_macos; then
-#   gpg --armor --export "$KEY_ID" | pbcopy
-# elif is_ubuntu || is_debian; then
-#   gpg --armor --export "$KEY_ID" | xclip -selection clipboard
-# fi
+if is_macos; then
+  gpg --armor --export "$KEY_ID" | pbcopy
+elif is_ubuntu || is_debian; then
+  gpg --armor --export "$KEY_ID" | xclip -selection clipboard
+fi
 
-# if is_macos; then
-#   open https://github.com/settings/keys
-# elif is_ubuntu || is_debian; then
-#   xdg-open https://github.com/settings/keys &>/dev/null
-# fi
+if is_macos; then
+  open https://github.com/settings/keys
+elif is_ubuntu || is_debian; then
+  xdg-open https://github.com/settings/keys &>/dev/null
+fi
 
-# echo 'press enter once you have pasted it in'
-# # shellcheck disable=SC2034
-# read -r CONTINUE
+echo 'press enter once you have pasted it in'
+# shellcheck disable=SC2034
+read -r CONTINUE
 
-# if is_macos; then
-#   echo 'pinentry-program /usr/local/bin/pinentry-mac' > ~/.gnupg/gpg-agent.conf
-# elif is_ubuntu || is_debian; then
-#   echo 'pinentry-program /usr/bin/pinentry-curses' >  ~/.gnupg/gpg-agent.conf
-# fi
+if is_macos; then
+  echo 'pinentry-program /usr/local/bin/pinentry-mac' > ~/.gnupg/gpg-agent.conf
+elif is_ubuntu || is_debian; then
+  echo 'pinentry-program /usr/bin/pinentry-curses' >  ~/.gnupg/gpg-agent.conf
+fi
--- source/20_gpg.sh
+++ source/20_gpg.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # Skip gpg until I figure out how to do subkeys properly
+return
 
-# Start the gpg agent if it hasn't started
+Start the gpg agent if it hasn't started
 
-# if [[ "$(pgrep gpg-agent)" == "" ]]; then
-#   eval "$(gpg-agent --daemon > /dev/null 2>&1)"
-#   echo "started gpg agent"
-# fi
+if [[ "$(pgrep gpg-agent)" == "" ]]; then
+  eval "$(gpg-agent --daemon > /dev/null 2>&1)"
+  echo "started gpg agent"
+fi
 
-# GPG_TTY=$(tty)
-# export GPG_TTY
+GPG_TTY=$(tty)
+export GPG_TTY
