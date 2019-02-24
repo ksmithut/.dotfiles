7a9fdb6319acffe460e8abe4d9e390a90b949009
ksmithut <ksmith.ut@gmail.com>
2019-02-24 07:21:56 -0700

    skip gpg stuff until I can figure out how to do subkeys propertly with github

--- source/20_gpg.sh
+++ source/20_gpg.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+
+# Skip gpg until I figure out how to do subkeys properly
+
 # Start the gpg agent if it hasn't started
 
-if [[ "$(pgrep gpg-agent)" == "" ]]; then
-  eval "$(gpg-agent --daemon > /dev/null 2>&1)"
-  echo "started gpg agent"
-fi
+# if [[ "$(pgrep gpg-agent)" == "" ]]; then
+#   eval "$(gpg-agent --daemon > /dev/null 2>&1)"
+#   echo "started gpg agent"
+# fi
 
-GPG_TTY=$(tty)
-export GPG_TTY
+# GPG_TTY=$(tty)
+# export GPG_TTY
