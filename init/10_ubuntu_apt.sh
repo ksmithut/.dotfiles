811b8e98da83ffcb5aaac5a4d0aefe1933316765
ksmithut <github@ksmithut.com>
2019-03-21 08:08:31 -0600

    remove extra line

--- init/10_ubuntu_apt.sh
+++ init/10_ubuntu_apt.sh
@@ -113,7 +113,6 @@ sudo apt-get update -y
 sudo apt-get install yarn -y
 
 # Erlang/Elixir
-# Erlang/Elixir
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb -O "$DOTFILES/caches/installers/erlang.deb"
 sudo dpkg -i "$DOTFILES/caches/installers/erlang.deb"
 sudo apt-get update
