c0e9e3a8e93175e35f6b9aaea3f43c6ec47597b5
ksmithut <github@ksmithut.com>
2019-03-21 08:04:33 -0600

    fix docker install

--- init/10_ubuntu_apt.sh
+++ init/10_ubuntu_apt.sh
@@ -54,12 +54,17 @@ sudo apt-get install -y \
 # sudo apt-get purge -y \
 #   firefox
 
+# Docker
+# Remove prior versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+# install docker
 curl -fsSL get.docker.com -o get-docker.sh
 chmod a+x get-docker.sh
 sudo sh get-docker.sh
 rm get-docker.sh
 sudo usermod -aG docker "$USER"
 
+# install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
