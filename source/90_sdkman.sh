c5bdfb892bbd7003919824b7675c9d4d3d5fc55a
ksmithut <github@ksmithut.com>
2019-06-20 13:07:49 -0600

    prevent error from happening in sdkman

--- source/90_sdkman.sh
+++ source/90_sdkman.sh
@@ -1,3 +1,3 @@
 # SDKMan requires it be sourced last
 export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh" || true
