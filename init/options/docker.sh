760f1104712ade06b11e713a32841e79d6209db6
ksmithut <github@ksmithut.com>
2019-06-20 10:50:50 -0600

    fix docker install

--- init/options/docker.sh
+++ init/options/docker.sh
@@ -9,10 +9,19 @@ if is_ubuntu; then
   # Remove prior versions
   sudo apt-get remove docker docker-engine docker.io containerd runc
   # install docker
-  curl -fsSL get.docker.com -o get-docker.sh
-  chmod a+x get-docker.sh
-  sudo sh get-docker.sh
-  rm get-docker.sh
+  sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io
   sudo usermod -aG docker "$USER"
 
   # install docker-compose
