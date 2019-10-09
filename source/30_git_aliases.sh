55eec7a9aeafafd56ade763a6fcf954c0d2227f0
ksmithut <github@ksmithut.com>
2019-10-08 19:36:32 -0600

    adds clone command to clone repo into code folder based on github repo url

--- source/30_git_aliases.sh
+++ source/30_git_aliases.sh
@@ -17,3 +17,19 @@ function ghclone() {
   git clone git@github.com:"$1" || return 1
   cd "$dirname" || return 1
 }
+
+function clone() {
+  if [[ "$1" == "" ]]; then
+    echo 'You must pass in a repo name'
+    return 1
+  fi
+  if [[ "$2" != "" ]]; then
+    git clone $1 $2
+    return 0
+  fi
+  folder_name="$(node -p "process.argv[1].replace(/^(git@|https:\/\/)/, '').replace(/\.git$/, '').split(/[:\/]/).join(path.sep)" $1)"
+  full_path=~/Code/"$folder_name"
+  mkdir -p "$full_path"
+  git clone "$1" "$full_path"
+  cd "$full_path"
+}
