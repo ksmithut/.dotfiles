ab368386ff211f961522f57a399657e1b47464e8
ksmithut <github@ksmithut.com>
2020-05-01 14:58:12 -0600

    updates docker version

--- init/options/docker.sh
+++ init/options/docker.sh
@@ -18,7 +18,7 @@ if is_ubuntu; then
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    disco \
+    eoan \
     stable"
   sudo apt-get update
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io
