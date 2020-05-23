784819bda7ef66c225883da2fe535f2aaf125574
ksmithut <github@ksmithut.com>
2020-05-23 15:12:29 -0600

    do not run gpg yet

--- install/programs/git-config/macos-linux.sh
+++ install/programs/git-config/macos-linux.sh
@@ -8,10 +8,9 @@ echo
 [[ $GITHUB_USER != "" ]] && git config --global user.name "$GITHUB_USER"
 [[ $GITHUB_EMAIL != "" ]] && git config --global user.email "$GITHUB_EMAIL"
 
-return
-GPG_SIGNING_KEY="$(gpg --list-secret-keys --keyid-format LONG | grep 'sec   ' | tail -1 | sed -n 's/.*\/\([^ ]*\) .*/\1/p')"
-if [ "${GPG_SIGNING_KEY}" != "" ]; then
-  git config --global user.signingkey "$GPG_SIGNING_KEY"
-  git config --global gpg.program gpg
-  git config --global commit.gpgsign true
-fi
+# GPG_SIGNING_KEY="$(gpg --list-secret-keys --keyid-format LONG | grep 'sec   ' | tail -1 | sed -n 's/.*\/\([^ ]*\) .*/\1/p')"
+# if [ "${GPG_SIGNING_KEY}" != "" ]; then
+#   git config --global user.signingkey "$GPG_SIGNING_KEY"
+#   git config --global gpg.program gpg
+#   git config --global commit.gpgsign true
+# fi
