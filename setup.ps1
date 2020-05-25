bbd34ea243d9b169539b1f678a3babd10251917e
ksmithut <github@ksmithut.com>
2020-05-24 19:36:30 -0600

    fix missing quotes

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 . "$PSScriptRoot\install\pre_windows.ps1"
 
 $env:Path = "C:\Program Files\Git\bin;$env:Path"
-$env:DOTFILES = $PSScriptRoot
-bash $PSScriptRoot\setup.sh"
+$env:DOTFILES = "$PSScriptRoot"
+bash "$PSScriptRoot\setup.sh"
