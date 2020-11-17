6c14c5960d38928c6f0ce27e447b5151cd4be312
ksmithut <github@ksmithut.com>
2020-11-16 20:07:37 -0700

    do not install settings sync extension because it is built into code now

--- install/programs/vscode/linux.sh
+++ install/programs/vscode/linux.sh
@@ -9,5 +9,3 @@ sudo apt-get install -f -y
 # https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
 echo 'fs.inotify.max_user_watches=524288' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
-
-code --install-extension Shan.code-settings-sync
--- install/programs/vscode/macos.sh
+++ install/programs/vscode/macos.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 brew cask install visual-studio-code
-
-code --install-extension Shan.code-settings-sync
