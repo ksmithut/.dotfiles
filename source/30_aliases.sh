96a6841e25a37658f40ffe5e6ac88355cf0aa724
ksmithut <github@ksmithut.com>
2021-01-10 07:27:55 -0700

    adds --greedy flag for cask upgrades

--- source/30_aliases.sh
+++ source/30_aliases.sh
@@ -137,7 +137,7 @@ if is_macos; then
   alias myip='ipconfig getifaddr en0; curl ipecho.net/plain; echo'
 
   # upgrade/update shortcuts
-  alias brew-upgrade='brew update && brew upgrade && brew upgrade --cask && brew cleanup'
+  alias brew-upgrade='brew update && brew upgrade && brew upgrade --cask --greedy && brew cleanup'
   alias upgrade='brew-upgrade'
 
   # show/hide hidden files in finder
