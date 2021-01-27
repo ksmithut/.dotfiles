05de88b5c9447de958b8ec9e4472f9ab71763fe4
ksmithut <github@ksmithut.com>
2021-01-27 11:40:31 -0700

    simplify ssh

--- install/programs/ssh-config/macos-linux.sh
+++ install/programs/ssh-config/macos-linux.sh
@@ -5,40 +5,23 @@ echo ''
 
 # Sets up ssh keys
 echo -n 'ssh key email (make it the github email if you wish to use with github): '; read -r SSH_EMAIL
-echo -n 'ssh key filename (id_rsa): '; read -r SSH_FILENAME
+echo -n 'ssh key filename (id_github): '; read -r SSH_FILENAME
 echo -n 'ssh key host (*): '; read -r SSH_HOST
 
-[[ $SSH_FILENAME == "" ]] && SSH_FILENAME="id_rsa"
+[[ $SSH_FILENAME == "" ]] && SSH_FILENAME="id_github"
 [[ $SSH_HOST == "" ]] && SSH_HOST="*"
 SSH_PATH="${HOME}/.ssh/${SSH_FILENAME}"
 
-ssh-keygen -t rsa -C "${SSH_EMAIL}" -f "${SSH_PATH}"
-
-echo ''
-echo 'Open https://github.com/settings/keys'
-echo 'press enter once you have signed into github'
-# shellcheck disable=SC2034
-read -r CONTINUE
-
-if [[ "${IS_MACOS}" == "1" ]]; then
-  pbcopy < "${SSH_PATH}.pub"
-elif [[ "${IS_LINUX}" == "1" ]]; then
-  xclip -selection clipboard < "${SSH_PATH}.pub"
-fi
-
-echo 'The public ssh key has been copied to your clipboard.'
-echo 'press enter once you have pasted it in'
-# shellcheck disable=SC2034
-read -r CONTINUE
+ssh-keygen -t ed25519 -C "${SSH_EMAIL}" -f "${SSH_PATH}"
 
 eval "$(ssh-agent -s > /dev/null 2>&1)"
-if [[ "${IS_MACOS}" == "1" ]]; then
+if [[ "$OSTYPE" =~ ^darwin ]]; then
   ssh-add -K "${SSH_PATH}"
 else
   ssh-add "${SSH_PATH}"
 fi
 
-if [[ "${IS_MACOS}" == "1" ]]; then
+if [[ "$OSTYPE" =~ ^darwin ]]; then
   echo "
 Host ${SSH_HOST}
   AddKeysToAgent yes
