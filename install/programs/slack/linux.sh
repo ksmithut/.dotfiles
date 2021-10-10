d3bc3c815b46de4adfee9a111eef9c8564603f27
ksmithut <github@ksmithut.com>
2021-10-10 07:53:34 -0600

    updates versions of docker-compose and slack

--- install/programs/slack/linux.sh
+++ install/programs/slack/linux.sh
@@ -2,6 +2,6 @@
 
 # UPDATE_VERSION - https://slack.com/downloads/instructions/ubuntu
 
-wget 'https://downloads.slack-edge.com/linux_releases/slack-desktop-4.11.1-amd64.deb' -O "$DOTFILES_INSTALLERS/slack.deb"
+wget 'https://downloads.slack-edge.com/releases/linux/4.20.0/prod/x64/slack-desktop-4.20.0-amd64.deb' -O "$DOTFILES_INSTALLERS/slack.deb"
 sudo dpkg -i "$DOTFILES_INSTALLERS/slack.deb"
 sudo apt-get install -f -y
