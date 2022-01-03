5443ac2fb0a649f7368fcff036c33aa79cf2acea
ksmithut <github@ksmithut.com>
2022-01-03 09:10:11 -0700

    improvements to installation of mongo-tools

--- install/programs/asdf-elixir/macos-linux.sh
+++ install/programs/asdf-elixir/macos-linux.sh
@@ -2,6 +2,8 @@
 
 . $HOME/.asdf/asdf.sh
 
+sudo apt-get install libssl-dev automake autoconf libncurses5-dev -y
+
 asdf plugin add erlang
 asdf install erlang latest
 asdf global erlang "$(asdf latest erlang)"
--- install/programs/asdf-mongo-tools/macos-linux.sh
+++ install/programs/asdf-mongo-tools/macos-linux.sh
@@ -2,6 +2,15 @@
 
 . $HOME/.asdf/asdf.sh
 
+sudo apt-get install libsasl2-dev libpcap-dev -y
+
+asdf plugin add golang
+asdf install golang 1.15.15
+asdf global golang 1.15.15
+
 asdf plugin add mongo-tools
 asdf install mongo-tools latest
 asdf global mongo-tools "$(asdf latest mongo-tools)"
+
+asdf install golang latest
+asdf global golang "$(asdf latest golang)"
