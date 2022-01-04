ab1a5200efd4d9269e940699079cd319fed3b45d
ksmithut <github@ksmithut.com>
2022-01-04 06:38:03 -0700

    fix elixir install

---
+++ install/programs/asdf-elixir/macos.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+. $HOME/.asdf/asdf.sh
+
+brew install autoconf openssl@1.1
+
+export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=$(brew --prefix openssl@1.1)"
+
+asdf plugin add erlang
+asdf install erlang latest
+asdf global erlang "$(asdf latest erlang)"
+
+asdf plugin add elixir
+asdf install elixir latest
+asdf global elixir "$(asdf latest elixir)"
