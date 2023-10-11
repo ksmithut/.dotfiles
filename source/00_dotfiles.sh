c2462ef6044e98dfbf74f7920d21380167d14922
ksmithut <github@ksmithut.com>
2023-10-11 08:12:56 -0600

    try exporting functions

--- source/00_dotfiles.sh
+++ source/00_dotfiles.sh
@@ -6,6 +6,7 @@
 is-macos () {
   [[ "$OSTYPE" =~ ^darwin ]] || return 1
 }
+export -f is-macos
 
 is-apple-silicon () {
   is-macos && [[ "$(arch)" == "arm64" ]] || return 1
@@ -14,6 +15,7 @@ is-apple-silicon () {
 is-ubuntu () {
   [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || [[ "$(cat /etc/issue 2> /dev/null)" =~ "Pop!_OS" ]] || return 1
 }
+export -f is-ubuntu
 
 is-zsh () {
   [[ "${ZSH_NAME}" == "zsh" ]] || return 1
@@ -22,14 +24,17 @@ is-zsh () {
 is-windows () {
   [[ "${OS}" == "Windows_NT" ]] || return 1
 }
+export -f is-windows
 
 is-linux () {
   [[ "$(which_os)" == "linux" ]] || return 1
 }
+export -f is-linux
 
 is-wsl () {
   [[ "$(cat /proc/version 2> /dev/null)" =~ microsoft ]] || return 1
 }
+export -f is-wsl
 
 dotfiles () {
   cd "$DOTFILES" || exit
