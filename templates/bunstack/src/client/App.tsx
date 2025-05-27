d27ebc9fd8579ef2061f1707628f648a4cc65917
ksmithut <github@ksmithut.com>
2025-05-27 07:53:26 -0600

    adjustments to bunstack

---
+++ templates/bunstack/src/client/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center p-3">
+      <h1 className="text-2xl font-bold">Hello World</h1>
+    </div>
+  )
+}
